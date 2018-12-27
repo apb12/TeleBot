b80f2eb1da8ca3c79cb94d3347b5be5d1b7407e7
asv <you@example.com>
2018-12-28 01:22:31 +0300

    make it better

--- src/main/java/Bdate.java
+++ src/main/java/Bdate.java
@@ -4,7 +4,7 @@ public class Bdate{
 
     String url = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/heroku_839483b01dcbc34?useUnicode=true&characterEncoding=UTF-8";
     String name="b3fd7df0612459";
-    String password="7ee5e40e1";
+    String password="7ee5e40e";
     Connection con;
 
     {
