414280e2e26a06976c22b68c3c9a89d036f2e97f
apb12 <46033369+apb12@users.noreply.github.com>
2019-04-12 22:45:13 +0300

    Update BaseDate.java

--- src/main/java/com/stas/telegrambots/BaseDate.java
+++ src/main/java/com/stas/telegrambots/BaseDate.java
@@ -7,8 +7,8 @@ import java.util.Map;
 class BaseDate  {
 
    private static final String url = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/heroku_839483b01dcbc34?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
-   private static final String name="b3fd7df0612459";
-   private static final String password="7ee5e40e";
+   private static final String name="";
+   private static final String password="";
 
 
     void update(String sql) throws SQLException  {
